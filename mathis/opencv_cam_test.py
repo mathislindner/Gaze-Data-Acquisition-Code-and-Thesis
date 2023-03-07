8b27860e9032e81555e8b646683d81b271d97c35
Mathis Lindner <mathis.lindner@gmail.com>
2023-03-07 11:11:19 +0100

    camera opens but can t receive frames

--- mathis/opencv_cam_test.py
+++ mathis/opencv_cam_test.py
@@ -3,16 +3,30 @@ import numpy as np
 import threading
 from uvc_stream import DEV_SETTINGS
 
+i = 0
 #stream 1 camera with cv2
-
-cap = cv.VideoCapture(-3, cv.CAP_DSHOW)
-cap.set(cv.CAP_PROP_FRAME_WIDTH, DEV_SETTINGS['pupil_invisible']['world']['frame_size'][0])
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, DEV_SETTINGS['pupil_invisible']['world']['frame_size'][1])
-cap.set(cv.CAP_PROP_FPS, DEV_SETTINGS['pupil_invisible']['world']['fps'])
+while i<100:
+    cap = cv.VideoCapture(i) #, cv.CAP_DSHOW)
+    
+    if cap.isOpened():
+        print(i)
+        break
+    i+=1
 
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+else:
+    print("Camera opened")
+#cap.set(cv.CAP_PROP_FRAME_WIDTH, DEV_SETTINGS['pupil_invisible']['world']['frame_size'][0])
+#cap.set(cv.CAP_PROP_FRAME_HEIGHT, DEV_SETTINGS['pupil_invisible']['world']['frame_size'][1])
+#cap.set(cv.CAP_PROP_FPS, DEV_SETTINGS['pupil_invisible']['world']['fps'])
+
+# set camera properties for eye_left
+cap.set(cv.CAP_PROP_FRAME_WIDTH, DEV_SETTINGS['pupil_invisible']['eye_left']['frame_size'][0])
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, DEV_SETTINGS['pupil_invisible']['eye_left']['frame_size'][1])
+cap.set(cv.CAP_PROP_FPS, DEV_SETTINGS['pupil_invisible']['eye_left']['fps'])
+
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
