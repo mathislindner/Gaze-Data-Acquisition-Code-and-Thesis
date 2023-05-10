5c8d4f385dc937c8af5ea7a57ddf3fd439ea936b
Mathis Lindner <mathis.lindner@gmail.com>
2023-05-10 21:35:34 +0200

    found scale in cleaner version of find scale.py

--- mathis/compare_poinclouds.py
+++ mathis/compare_poinclouds.py
@@ -18,6 +18,12 @@ def get_colmap_dense_model(recording_path):
     cameras, images, points_3d = read_write_model.read_model(path, ".bin")
     return cameras, images, points_3d
 
+def get_depth_distances_array(recording_path):
+    depth_camera_distances = np.load(os.path.join(recording_path, "depth_array.npz"))
+    file_name = depth_camera_distances.files[0]
+    return depth_camera_distances[file_name][90] #depth_camera_distances[file_name][90] is the depth array of the 90th frame
+    
+
 def get_depth_camera_pointcloud(recording_path, nr_points=20000):
     path = os.path.join(recording_path,"meshed_depth_camera.ply")
     mesh = o3d.io.read_triangle_mesh(path)
@@ -42,7 +48,7 @@ def draw_pointclouds(pointclouds):
     o3d.visualization.draw_plotly(pointclouds)
 
 #returns the colmap image dict
-def get_depth_image(images):
+def get_colmap_depth_image(images):
     depth_image_id = None
     for image_id in images:
         if images[image_id].name == "depth_rgb.png":
@@ -51,28 +57,41 @@ def get_depth_image(images):
     return images[image_id]
 
 #returns the depths of the points thatare non zero in from the depth array
-def get_depths_of_nonzero(depth_array, images, indices_of_nonzero):
-    pixel_location_of_visible_points_by_depth_camera = depth_array.xys[indices_of_nonzero]
-    #round pixel locations
-    pixel_location_of_visible_points_by_depth_camera = np.round(pixel_location_of_visible_points_by_depth_camera).astype(int)
-    depths = []
-    for pixel_location in pixel_location_of_visible_points_by_depth_camera:
-        depths.append(depth_array.depths[pixel_location[1], pixel_location[0]])
-    depths = np.array(depths)
-    return depths
+def get_depths_and_colmap_point_ids(colmap_depth_image, colmap_points, depth_array):
+    #get the visible points
+    visible_colmap_point_ids, depths = [], []
+    for point_ids, pixel_location in zip(colmap_depth_image.point3D_ids, colmap_depth_image.xys):
+        if point_ids != -1:
+            pixel_location = np.round(pixel_location).astype(int) #round pixel locations
+            x = pixel_location[0]
+            y = pixel_location[1]
+            if depth_array[y, x] != 0:
+                visible_colmap_point_ids.append(point_ids)
+                depths.append(depth_array[y, x])
+                
+    return np.array(depths), np.array(visible_colmap_point_ids)
 
     
 
-def get_scale(colmap_cameras, colmap_images, colmap_points, depth_camera_distances):
-    depth_array = get_depth_image(colmap_images)
-    indices_of_nonzero = [i for i in range (len(depth_array.point3D_ids)) if depth_array.point3D_ids[i] != -1]
-    depths_of_nonzero = get_depths_of_nonzero(colmap_images, indices_of_nonzero)
-    visible_colmap_points = colmap_points[indices_of_nonzero].xyz
-    camera_position = get_depth_image(colmap_images).tvec
-
+def get_scale(colmap_cameras, colmap_images, colmap_points, depth_array):
+    colmap_depth_image = get_colmap_depth_image(colmap_images)
+    depths, point_ids = get_depths_and_colmap_point_ids(colmap_depth_image, colmap_points, depth_array)
+    visible_3Dpoints_by_depth_camera = np.array([colmap_points[point_id].xyz for point_id in point_ids])
+    camera_position = colmap_depth_image.tvec
     #calculate distance between camera and points
-    distances = np.linalg.norm(visible_colmap_points - camera_position, axis=1)
+    distances = np.linalg.norm(visible_3Dpoints_by_depth_camera - camera_position, axis=1)
     #calculate scale
-    scale = np.mean(depths_of_nonzero / distances)
-    return scale
+    scale = np.mean(depths / distances)
+    return scale / 1000 #convert to meters (because the pointcloud is in meters)
+############################################################################################################
+sparse_cameras, sparse_images, sparse_points = get_colmap_sparse_model(recording_path)
+dense_cameras, dense_images, dense_points = get_colmap_dense_model(recording_path)
+depth_distances_array = get_depth_distances_array(recording_path)
+
+
+sparse_scale = get_scale(sparse_cameras, sparse_images, sparse_points, depth_distances_array)
+dense_scale = get_scale(dense_cameras, dense_images, dense_points, depth_distances_array)
+
+print("sparse scale: ", sparse_scale)
+print("dense scale: ", dense_scale)
 
