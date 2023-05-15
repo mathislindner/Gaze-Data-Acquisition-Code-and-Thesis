45ba8ad0477ce65240f82b55ac6c4a3a88850059
Mathis Lindner <mathis.lindner@gmail.com>
2023-05-15 10:41:51 +0200

    added the automatic reconstructor to the pipeline

--- mathis/colmap_executer.py
+++ mathis/colmap_executer.py
@@ -5,6 +5,48 @@ import time
 import shutil
 import subprocess
 
+#FIXME: will probably run into issues if we have the recordings ina different folder than on scratch net
+def run_colmap_automatic_reconstructor(recording_id):
+    current_working_dir = os.getcwd()
+    runcolmap_batch_file = os.path.join(current_working_dir, 'mathis', "run_colmap.sh")
+    
+    recording_folder = os.path.join(recordings_folder,str(recording_id))
+
+    undistorted_world_camera_folder = os.path.join(recording_folder, "PI_world_v1_ps1_undistorted")
+    #distorted_world_camera_folder = os.path.join(recording_folder, "PI_world_v1_ps1")
+    depth_camera_folder = os.path.join(recording_folder, "rgb_pngs")
+
+    colmap_ws_folder = os.path.join(recording_folder, "colmap_ws")
+    colmap_ws_images_folder = os.path.join(colmap_ws_folder, "all_images")
+    colmap_ws_out = os.path.join(colmap_ws_folder, "automatic_recontructor_out")
+    #colmap_log_dir = os.path.join(colmap_ws_folder, "log")
+
+    #create the colmap workspace folder
+    #TODO: create 2 subfolders in images, one for the world camera and one for the depth camera
+    if not os.path.exists(colmap_ws_folder):
+        os.makedirs(colmap_ws_folder)
+    if not os.path.exists(colmap_ws_images_folder):
+        os.makedirs(colmap_ws_images_folder)
+    if not os.path.exists(colmap_ws_out):
+        os.makedirs(colmap_ws_out)
+
+
+    #copy the world images to the colmap workspace folder 3 frames per second
+    copy_frames_to_new_folder(undistorted_world_camera_folder, colmap_ws_images_folder, step = 10)
+
+    #copy one depth image to the colmap workspace folder (3secs after recording start)
+    shutil.copy(os.path.join(depth_camera_folder, "95.png"), colmap_ws_images_folder)
+    #rename the depth image to depth_rgb.png to not have the same name as the world camera images
+    os.rename(os.path.join(colmap_ws_images_folder, "95.png"), os.path.join(colmap_ws_images_folder, "depth_rgb.png"))
+    
+    #run command for colmapsh
+    command = "sbatch" + " " + runcolmap_batch_file + " " + recording_folder
+    print(command)
+    os.system(command)
+
+run_colmap_automatic_reconstructor('ff0acdab-123d-41d8-bfd6-b641f99fc8eb_copy')
+
+
 def run_colmap(recording_id):
     current_working_dir = os.getcwd()
     runcolmap_batch_file = os.path.join(current_working_dir, "run_colmap.sh")
@@ -12,7 +54,7 @@ def run_colmap(recording_id):
     recording_folder = os.path.join(recordings_folder,str(recording_id))
 
     undistorted_world_camera_folder = os.path.join(recording_folder, "PI_world_v1_ps1_undistorted")
-    distorted_world_camera_folder = os.path.join(recording_folder, "PI_world_v1_ps1")
+    #distorted_world_camera_folder = os.path.join(recording_folder, "PI_world_v1_ps1")
     depth_camera_folder = os.path.join(recording_folder, "rgb_pngs")
 
     colmap_ws_folder = os.path.join(recording_folder, "colmap_ws")
@@ -42,7 +84,7 @@ def run_colmap(recording_id):
     #copy the world images to the colmap workspace folder 3 frames per second
     copy_frames_to_new_folder(undistorted_world_camera_folder, colmap_world_camera_folder_undistorted, step = 10)
     #copy distorted images to new folder
-    copy_frames_to_new_folder(distorted_world_camera_folder, colmap_world_camera_folder_distorted, step = 10)
+    #copy_frames_to_new_folder(distorted_world_camera_folder, colmap_world_camera_folder_distorted, step = 10)
     #copy_frames_to_new_folder(world_camera_folder, colmap_ws_images_folder, step = 10)
 
     #copy one depth image to the colmap workspace folder (3secs after recording start)
@@ -54,7 +96,7 @@ def run_colmap(recording_id):
     #FIXME make sure executed in the right folder, somehow can t pass runcolmap_batch_file as argument (removes backslashes)
     #os.system("sbatch" + " " + "run_colmap.sh" + " " + recording_folder)
 
-run_colmap("ff0acdab-123d-41d8-bfd6-b641f99fc8eb")
+#run_colmap("ff0acdab-123d-41d8-bfd6-b641f99fc8eb")
 
 def convert_vectors_to_true_scale(recording_id):
     recording_folder = os.path.join(recordings_folder,str(recording_id))
--- mathis/run_colmap.sh
+++ mathis/run_colmap.sh
@@ -1,57 +1,15 @@
 #!/bin/bash
-#SBATCH --output=/scratch_net/snapo/mlindner/docs/test_cluster/f5b09068-d395-493c-80e6-7eb0cbb6478a/colmap_ws/log/%j.out
-#SBATCH --gres=gpu:1
+#SBATCH --output=/scratch_net/snapo/mlindner/docs/gaze_data_acquisition/mathis/colmap_log/%j.out
+#SBATCH --gres=gpu:1 
 #SBATCH --mem=32G
 
 #first argument should be the recording folder
 recordingfolder=$1
 
 WS_PATH=$recordingfolder/colmap_ws
-DATASET_PATH=$WS_PATH/dataset
 
 echo $WS_PATH
-echo $DATASET_PATH
 
-colmap feature_extractor \
-   --database_path $DATASET_PATH/database.db \
-   --image_path $DATASET_PATH/images \
-   --ImageReader.single_camera_per_folder true \
-   --ImageReader.single_camera_per_image false
-
-colmap exhaustive_matcher \
-   --database_path $DATASET_PATH/database.db
-
-mkdir $WS_PATH/colmap_out
-mkdir $WS_PATH/colmap_out/sparse
-
-colmap mapper \
-    --database_path $DATASET_PATH/database.db \
-    --image_path $DATASET_PATH/images \
-    --output_path $WS_PATH/colmap_out
-
-mkdir $WS_PATH/colmap_out/dense
-
-colmap image_undistorter \
-    --image_path $DATASET_PATH/images \
-    --input_path $WS_PATH/colmap_out/sparse/0 \
-    --output_path $WS_PATH/colmap_out/dense \
-    --output_type COLMAP \
-
-$ colmap patch_match_stereo \
-    --workspace_path $WS_PATH/colmap_out/dense \
-    --workspace_format COLMAP \
-    --PatchMatchStereo.geom_consistency true
-
-$ colmap stereo_fusion \
-    --workspace_path $WS_PATH/colmap_out/dense \
-    --workspace_format COLMAP \
-    --input_type geometric \
-    --output_path $WS_PATH/colmap_out/dense/fused.ply
-
-$ colmap poisson_mesher \
-    --input_path $WS_PATH/colmap_out/dense/fused.ply \
-    --output_path $WS_PATH/colmap_out/dense/meshed-poisson.ply
-
-$ colmap delaunay_mesher \
-    --input_path $WS_PATH/colmap_out/dense \
-    --output_path$WS_PATH/colmap_out/dense/meshed-delaunay.ply
+colmap automatic_reconstructor \
+    --workspace_path $WS_PATH/automatic_recontructor_out \
+    --image_path $WS_PATH/all_images 
