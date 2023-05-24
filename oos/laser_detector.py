925a56de4d143e92e4a5afe9649a2e9cd2b2cc5a
Mathis Lindner <mathis.lindner@gmail.com>
2023-05-24 11:59:57 +0200

    find laser work in progress

--- oos/laser_detector.py
+++ oos/laser_detector.py
@@ -1,28 +1,36 @@
+#add the path to the dependencies folder
+import sys
+sys.path.append("/scratch_net/snapo/mlindner/docs/gaze_data_acquisition")
 import cv2
 import numpy as np
-from constants import *
 import os
 import glob
 from time import sleep
-from colmap_testing.colmap_helpers import read_write_model
-from compare_poinclouds import get_colmap_dense_model, get_colmap_sparse_model, get_depth_distances_array, get_colmap_depth_camera
+from dependencies.constants import *
+from dependencies.colmap_helpers import read_write_model
+#from compare_poinclouds import get_colmap_dense_model, get_colmap_sparse_model, get_depth_distances_array, get_colmap_depth_camera
 import pyrealsense2 as rs
 
-def get_pixel_location_of_laser(image_path):
+#rectangle: top left(x,y), top right(x,y), bottom left(x,y), bottom right(x,y)
+def find_laser_in_image(image_array, rectangle):
     #read the image
-    image = cv2.imread(image_path)
-    cv2.imshow("image", image)
+    image = image_array
+    #set to black everything outside the rectangle
+    mask = np.zeros(image.shape[:2], dtype="uint8")
+    mask = cv2.drawContours(mask, [rectangle], -1, 255, -1)
+    #apply the mask
+    masked_image = cv2.bitwise_and(image, image, mask=mask)
+    #show the masked image
+    cv2.imshow("masked_image", masked_image)
     cv2.waitKey(0)
     #initialize the center of the laser
-    x, y = 0, 0
+    x, y = -1, -1
     #define the range of the laser in HSV
     lower = 60
     upper = 255
     #convert the image to HSV
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(masked_image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, (lower, lower, lower), (upper, upper, upper))
-    cv2.imshow("mask", mask)
-    cv2.waitKey(0)
     #find the contours of the laser
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     #if there are contours
@@ -40,9 +48,65 @@ def get_pixel_location_of_laser(image_path):
     cv2.waitKey(0)
     return (x,y)
 
-laser_image_path = os.path.join(recordings_folder, "laser_test.jpeg")
-x,y = get_pixel_location_of_laser(laser_image_path)
-print(x,y)
+#get aruco projection rectangle ids to corners mapping
+#0: top left, 1: top right, 2: bottom left, 3: bottom right
+def get_aruco_rectangle(image_array):
+    image=image_array
+    #convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #aruco dictionary
+    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+    #aruco parameters
+    parameters = cv2.aruco.DetectorParameters_create()
+    #detect the aruco markers
+    corners, ids, rejected = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    #if there are markers
+    if ids is not None and len(ids) > 3:
+        #draw the markers
+        image = cv2.aruco.drawDetectedMarkers(image, corners, ids)
+        cv2.imwrite('aruco_detected.png', image)
+        #get the corners of the markers
+        corners = np.squeeze(corners)
+        #get the ids of the markers
+        ids = np.squeeze(ids)
+        #sort the corners by ids
+        corners = corners[ids.argsort()]
+        #transform rectangle to top left(x,y), top right(x,y), bottom left(x,y), bottom right(x,y)
+        #FIXME when putting the aruco markers, put them in the right order (top left, top right, bottom right, bottom left) and then remove this
+        resort_indexes = [0, 1, 3, 2]
+        corners = corners[resort_indexes]
+        #get the inner corners of each marker (top left corner is bottom right, top right corner is bottom left, bottom left corner is top right, bottom right corner is top left)
+        rectangle_corners = np.zeros((4,2))
+        for i in range(4):
+            rectangle_corners[i] = corners[i][(i+2) % 3]
+            print(corners[i][(i+2) % 3])
+        return rectangle_corners.astype(np.int32)
+        
+
+def get_2D_laser_position(image_array):
+    aruco_rectangle = get_aruco_rectangle(image_array)
+    position = find_laser_in_image(image_path, aruco_rectangle)
+    return position
+
+def get_depth_of_pixel(image_path, pixel):
+    #get the depth of the pixel
+    depth = image_path[pixel[1], pixel[0]]
+    return depth
+
+def get_3D_laser_position(image_array, depth_array_corresponding_to_image):
+    #get the image path
+    laser_2D_position = get_2D_laser_position(image_array)
+    #get the depth of the pixel
+    depth_of_pixel = get_depth_of_pixel(depth_array_corresponding_to_image, laser_2D_position)
+    #get the 3D coordinates of the pixel
+    coordinates_3D = rs.deproject_pixel_to_point(laser_2D_position, depth_of_pixel)
+    return coordinates_3D
+
+
+#i=205
+#image_path = os.path.join(recordings_folder, "b6a73239-5f5b-4fad-ad65-fcefb27ba4d8", "rgb_pngs", "{}.png".format(i))
+#x,y = get_2D_laser_position(image_path)
+#print(x,y)
 
 def depth_camera_pixel_to_3D(pixel, depths, recording_path):
     #import colmap camera parameters
