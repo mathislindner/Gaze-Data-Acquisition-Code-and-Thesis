b2187cb61b0329db81bebe757cd95ac38e97c65d
Mathis Lindner <mathis.lindner@gmail.com>
2023-06-15 16:48:59 +0200

    also plot yaw pitch roll

--- oos/visualize_colmap_path_rawpitchroll.py
+++ oos/visualize_colmap_path_rawpitchroll.py
@@ -19,6 +19,8 @@ def get_image_positions(recording_id):
         translation_vectors.append(image.tvec)
     return translation_vectors
 
+
+
 def get_2d_image_positions(recording_id):
     translation_vectors = get_image_positions(recording_id)
     xyz = np.array(translation_vectors)
@@ -26,6 +28,31 @@ def get_2d_image_positions(recording_id):
     #connect the points
     xy = np.vstack((xy, xy[0]))
     return xy
+
+def get_image_quaternions(recording_id):
+    export_folder = os.path.join(exports_folder,recording_id)
+    colmap_path = os.path.join(export_folder, "colmap_EM_export")
+    cameras, images, points = read_write_model.read_model(colmap_path, ".txt")
+    quaternions = []
+    #sort iamge ids by image name
+    image_ids = sorted(images.keys(), key=lambda x: images[x].name)
+    for image_id in image_ids:
+        image = images[image_id]
+        quaternions.append(image.qvec)
+    return quaternions
+def get_yaw_pitch_roll_from_one_quaternion(q):
+    yaw = np.arctan2(2*(q[0]*q[1] + q[2]*q[3]), 1 - 2*(q[1]**2 + q[2]**2))
+    pitch = np.arcsin(2*(q[0]*q[2] - q[3]*q[1]))
+    roll = np.arctan2(2*(q[0]*q[3] + q[1]*q[2]), 1 - 2*(q[2]**2 + q[3]**2))
+    return np.array([yaw, pitch, roll])
+
+#return numpy array of shape (n,3) where n is the number of images
+def get_yaw_pitch_roll_from_quaternion(qs):
+    yaw_pitch_roll = np.zeros((len(qs),3))
+    for i,q in enumerate(qs):
+        yaw_pitch_roll[i] = get_yaw_pitch_roll_from_one_quaternion(q)
+    return yaw_pitch_roll
+
 
 def plot_one_path():
     recording_ids = os.listdir(exports_folder)
@@ -57,5 +84,35 @@ def plot_a_few_paths():
         ax[i//3,i%3].set_title(recording_ids[i])
     plt.show()
 
-plot_one_path()
+def plot_one_yaw_pitch_roll():
+    recording_ids = os.listdir(exports_folder)
+    recording_ids = os.listdir(exports_folder)
+    #if the recording does not have colmap_EM_export/cameras.txt remove it from the list
+    recording_ids = [recording_id for recording_id in recording_ids if os.path.exists(os.path.join(exports_folder, recording_id, "colmap_EM_export", "cameras.txt"))]
+    random.shuffle(recording_ids)
+    yaw_pitch_roll = get_yaw_pitch_roll_from_quaternion(get_image_quaternions(recording_ids[0]))
+    #create a figure with 3 subplots in polar coordinates
+    fig, ax = plt.subplots(3,1, subplot_kw=dict(projection='polar'))
+    #add a title
+    plt.title(recording_ids[0][:16])
+    #change figure size
+    fig.set_size_inches(5, 15)
+    for i in range(3):
+        ax[i].scatter(yaw_pitch_roll[:,i], np.arange(len(yaw_pitch_roll)))
+        ax[i].set_theta_zero_location("N")
+        #add a title
+        ax[i].set_title(["Yaw", "Pitch", "Roll"][i])
+        ax[i].set_yticklabels([])
+
+    
+    #put space between the subplots
+    plt.subplots_adjust(hspace=0.5)
+    
+
+
+
+    plt.savefig("plots/yaw_pitch_roll{}.png".format(recording_ids[0]))
+
+plot_one_yaw_pitch_roll()
+#plot_one_path()
 #plot_a_few_paths()
